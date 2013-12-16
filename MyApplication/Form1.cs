5c4240941f57ed9aff7bfe87cd07e2e0df52eb70
butmasaru <butmasaru+git@gmail.om>
2013-12-16 16:28:34 +0900

    ボタンを足した

--- MyApplication/Form1.cs
+++ MyApplication/Form1.cs
@@ -16,5 +16,10 @@ namespace MyApplication
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
