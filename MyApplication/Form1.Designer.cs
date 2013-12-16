ccd5429a9015e7346b2169282b57fde4cbb64ad6
butmasaru <butmasaru+git@gmail.om>
2013-12-16 18:11:48 +0900

    aaaaaa

--- MyApplication/Form1.Designer.cs
+++ MyApplication/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(108, 101);
+            this.button1.Location = new System.Drawing.Point(164, 162);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
